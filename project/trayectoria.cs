f23b6a4cd50c648f4fcc397f8dbcda9fded80b29
jorland <jorland29rp@hotmail.com>
2015-07-28 10:40:39 -0600

    Se agrego la funcionabilidad de abrir el mapa con parametos especificos
    La clase recibe parametros por medio el cosntructor y con esos parametos
    se crea la url parametros de paisOrigen y paisDestino despues el mapa se
    muestra en el componente wbBrower

--- project/trayectoria.cs
+++ project/trayectoria.cs
@@ -13,9 +13,15 @@ namespace project
 {
     public partial class trayectoria : Form
     {
+        private string PaisOrigen;
+        private string paisDestino;
+
+        //contructor recibe los paises destino y origen 
         public trayectoria(string PaisOrigen,string paisDestino)
         {
             InitializeComponent();
+            this.PaisOrigen = PaisOrigen;
+            this.paisDestino = paisDestino;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,7 +36,14 @@ namespace project
 
         private void trayectoria_Load(object sender, EventArgs e)
         {
-            //webBrowser1.Navigate(Application.StartupPath + @"\trayectoria.html");
+            string url = string.Format("http://pruebamaps.webatu.com/prueba.html?po={0}&pd={1}",PaisOrigen,paisDestino);
+
+            webBrowser1.Navigate(url);//abre el sitio web en el componente webBrowser
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
